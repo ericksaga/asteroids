ee3784202e5ddfcc7a5376245c0931692eb89446
Enmanuel Marcial Medina Martinez <erickmedina09@gmail.com>
2018-02-02 22:07:40 -0400

    added vector2

---
+++ asteroids_code/BoilerPlate/vector2.cpp
@@ -0,0 +1,130 @@
+#include "vector2.hpp"
+
+vector2::vector2()
+{
+	x = 0.0f;
+	y = 0.0f;
+	length = 0.0f;
+}
+
+vector2::vector2(float cord_x, float cord_y)
+{
+	x = cord_x;
+	y = cord_y;
+	length = Length();
+}
+
+vector2::vector2(float value)
+{
+	x = value;
+	y = value;
+	length = Length();
+}
+
+//functions
+
+float vector2::Length()
+{
+	return sqrt(x * x + y * y);
+}
+
+float vector2::squaredlength()
+{
+	return x * x + y * y;
+}
+
+float vector2::normalize()
+{
+	Length();
+	float scale = 1.0f / length;
+	x *= scale;
+	y *= scale;
+	return length;
+}
+
+//Overload
+
+vector2 vector2::operator+(const vector2& val)
+{
+	return vector2(x + val.x, y + val.y);
+}
+
+vector2 vector2::operator-(const vector2& val)
+{
+	return vector2(x - val.x, y - val.y);
+}
+
+vector2 vector2::operator*(const vector2& val)
+{
+	return vector2(x * val.x, y * val.y);
+}
+
+vector2 vector2::operator/(const vector2& val)
+{
+	if (val.x != 0 && val.y != 0)
+	{
+		return vector2(x / val.x, y / val.y);
+	}
+	return 0;
+}
+
+bool vector2::operator==(const vector2& val)
+{
+	return (val.x == x) && (val.y == y);
+}
+bool vector2::operator!=(const vector2& val)
+{
+	return (val.x != x) || (val.y != y);
+}
+
+vector2& vector2::operator=(const vector2& val)
+{
+	if (*this == val)
+	{
+		return *this;
+	}
+	x = val.x;
+	y = val.y;
+	return *this;
+}
+
+vector2& vector2::operator+=(const vector2& val)
+{
+	x = val.x + x;
+	y = val.y + y;
+	return *this;
+}
+
+vector2& vector2::operator-=(const vector2& val)
+{
+	x = x - val.x;
+	y = y - val.y;
+	return *this;
+}
+
+vector2& vector2::operator*=(const vector2& val)
+{
+	x = x * val.x;
+	y = y * val.y;
+	return *this;
+}
+
+vector2& vector2::operator/=(const vector2& val)
+{
+	if (val.x != 0 && val.y != 0)
+	{
+		x = x / val.x;
+		y = y / val.y;
+	} 
+	return *this;
+}
+
+vector2 operator*(float mult, const vector2& vect) 
+{
+	return vector2(mult * vect.x, mult * vect.y);
+}
+
+vector2 operator*(const vector2& vect, float mult)
+{
+	return vector2(mult * vect.x, mult * vect.y);
+}
---
+++ asteroids_code/BoilerPlate/vector2.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cmath>
+
+struct vector2 {
+	
+	//members
+	float x;
+	float y;
+	float length;
+
+	//constructor
+	vector2();
+	vector2(float, float);
+	vector2(float);
+
+	//functions
+
+	float Length();
+	float squaredlength();
+	float normalize();
+	
+	//Overload
+
+	vector2 operator+(const vector2&);
+	vector2 operator-(const vector2&);
+	vector2 operator*(const vector2&);
+	vector2 operator/(const vector2&);
+	bool operator==(const vector2&);
+	bool operator!=(const vector2&);
+	vector2& operator=(const vector2&);
+	vector2& operator+=(const vector2&);
+	vector2& operator-=(const vector2&);
+	vector2& operator*=(const vector2&);
+	vector2& operator/=(const vector2&);
+	friend vector2 operator*(float, const vector2&);
+	friend vector2 operator*(const vector2&, float);
+
+
+
+};
